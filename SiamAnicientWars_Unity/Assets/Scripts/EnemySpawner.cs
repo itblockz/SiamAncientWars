220639136b660d3b8da4255e7b19fa9010c4d300
ItBlockZ <nontapat.boss@gmail.com>
2024-02-22 01:16:42 +0700

    add tank enemy

--- SiamAnicientWars_Unity/Assets/Scripts/EnemySpawner.cs
+++ SiamAnicientWars_Unity/Assets/Scripts/EnemySpawner.cs
@@ -13,6 +13,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float difficultyScalingFactor = 0.75f;
+    [SerializeField] private float enemiesPerSecondCap = 15f;
 
     [Header("Events")]
     public static UnityEvent onEnemyDestroy = new UnityEvent();
@@ -21,6 +22,7 @@ public class EnemySpawner : MonoBehaviour
     private float timeSinceLastSpawn;
     private int enemiesAlive;
     private int enemiesLeftToSpawn;
+    private float eps; // Enemies Per Second
     private bool isSpawning = false;
 
     private void Awake() {
@@ -35,7 +37,7 @@ public class EnemySpawner : MonoBehaviour
         if (!isSpawning) return;
         timeSinceLastSpawn += Time.deltaTime;
 
-        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0) {
+        if (timeSinceLastSpawn >= (1f / eps) && enemiesLeftToSpawn > 0) {
             SpawnEnemy();
             enemiesLeftToSpawn--;
             enemiesAlive++;
@@ -55,6 +57,7 @@ public class EnemySpawner : MonoBehaviour
         yield return new WaitForSeconds(timeBetweenWaves);
         isSpawning = true;
         enemiesLeftToSpawn = EnemiesPerWave();
+        eps = EnemiesPerSecond();
     }
 
     private void EndWave() {
@@ -65,12 +68,18 @@ public class EnemySpawner : MonoBehaviour
     }
 
     private void SpawnEnemy() {
-        GameObject prefabToSpawn = enemyPrefabs[0];
+        int index = Random.Range(0, enemyPrefabs.Length);
+        GameObject prefabToSpawn = enemyPrefabs[index];
         Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
 
     }
 
     private int EnemiesPerWave() {
         return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
+    
+    private float EnemiesPerSecond() {
+        return Mathf.Clamp(enemiesPerSecond * Mathf.Pow(currentWave, 
+        difficultyScalingFactor), 0f, enemiesPerSecondCap);
     }
 }
