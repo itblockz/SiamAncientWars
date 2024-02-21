2eb07aa870e738df269396fa6397c90279829b13
ItBlockZ <nontapat.boss@gmail.com>
2024-02-21 22:36:48 +0700

    add shop

--- SiamAnicientWars_Unity/Assets/Scripts/BuildManager.cs
+++ SiamAnicientWars_Unity/Assets/Scripts/BuildManager.cs
@@ -7,7 +7,7 @@ public class BuildManager : MonoBehaviour
     public static BuildManager main;
 
     [Header("References")]
-    [SerializeField] private GameObject[] towerPrefabs;
+    [SerializeField] private Tower[] towers;
 
     private int selectedTower = 0;
 
@@ -15,7 +15,11 @@ public class BuildManager : MonoBehaviour
         main = this;
     }
 
-    public GameObject GetSelectedTower() {
-        return towerPrefabs[selectedTower];
+    public Tower GetSelectedTower() {
+        return towers[selectedTower];
+    }
+
+    public void SetSelectedTower(int _selectedTower) {
+        selectedTower = _selectedTower;
     }
 }
---
+++ SiamAnicientWars_Unity/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI currencyUI;
+    [SerializeField] private Animator anim;
+
+    private bool isMenuOpen = true;
+
+    public void ToggleMenu() {
+        isMenuOpen = !isMenuOpen;
+        anim.SetBool("MenuOpen", isMenuOpen);
+    }
+
+    private void OnGUI() {
+        currencyUI.text = LevelManager.main.currency.ToString();
+    }
+}
